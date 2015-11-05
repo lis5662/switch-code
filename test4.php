762fa42892bd5c69a1252350606891828afd6356
lis5662 <lis5662@gmail.com>
2015-11-05 16:42:47 +0200

    my switch code

---
+++ test4.php
@@ -0,0 +1,41 @@
+<?php
+
+$num = 4;
+    
+switch($num)
+    {
+        case 7:
+            echo "Вы добавили в корзину Istick 100w";
+        break;
+            
+        case 2:
+            echo "Вы добавили в корзину Subtank Mini";
+        break;   
+            
+        case 4:
+            echo "Вы добавили в корзину Box Abaddon El Diablo";
+        break;   
+    }
+
+?>
+
+
+<br>
+
+
+<?php
+
+$a = "2015";
+
+switch($a)
+{
+    case 1990: echo"1990 год->";
+    case 1995: echo"1995 год->"; 
+    case 2000: echo"2000 год->";
+    case 2015: echo"2015 год->";
+    case 2008: echo"2008 год";     
+    
+}
+
+
+?>
